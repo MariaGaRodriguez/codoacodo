6434b65f885a10e8462704409eb25d18f27f2f02
rociogh <rociogh131@gmail.com>
2022-10-25 15:20:04 -0300

    modifico el carrusel

--- js/form.js
+++ js/form.js
@@ -1,19 +1,20 @@
 (function () {
-    'use strict'
-  
-    // Obtener todos los formularios a los que queremos aplicar estilos de validación de Bootstrap personalizados
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Bucle sobre ellos y evitar el envío
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-          else{alert("Formulario enviado con éxito. Le responderemos a la brevedad") }    
-          form.classList.add('was-validated') }, false)
-        
-      })
-  })()
+  'use strict'
+
+  // Obtener todos los formularios a los que queremos aplicar estilos de validación de Bootstrap personalizados
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Bucle sobre ellos y evitar el envío
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+        else{alert("Formulario enviado con éxito. Le responderemos a la brevedad") }
+        this.reset()    
+        form.classList.add('was-validated') }, false)
+      
+    })
+})()
